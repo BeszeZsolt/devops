77dbc74d3c59cbcca2ff3559b8b5993ec2b9cb54
Zsolt Besze <zsolt.besze@stud.uni-corvinus.hu>
2024-11-26 18:36:42 +0100

    welcome üzenet módosítva

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5078f2d2ded8e0205c600e34c404cf997e0c4676")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1401e924985169accf32a67021328c20c1c6399e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
